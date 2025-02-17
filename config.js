d51864722f4ae2768fa62fa46a699ec9977e663c
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-02-17 23:37:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ESHU-MD=sK93GCyB#tbfgv5rKc1ZrTB4DjgudHDd_tCL79sGekc68mcHRMms",
+SESSION_ID: process.env.SESSION_ID || "ESHU-MD=SILENT-SOBX-MD~KQ5HXYpY#HSWY1GDMzV7GVlwbvWvN-Rv7DlcICI7miDvYWiw3vYw",
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", // true or false
