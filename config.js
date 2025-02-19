743bcc9d9900e89daab579eeee21154f03fe7194
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-02-19 09:32:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ESHU-MD=SILENT-SOBX-MD~KQ5HXYpY#HSWY1GDMzV7GVlwbvWvN-Rv7DlcICI7miDvYWiw3vYw",
+SESSION_ID: process.env.SESSION_ID || "ESHU-MD=taMnzTCa#Az2NCk-eRJS8J4FCMP4xLcicVR8jy4P118T6J7PaxYM",
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", // true or false
-BOT_NAME: process.env.BOT_NAME || "ESHU 𝙼𝙳 V1",
+BOT_NAME: process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍 𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐕2",
 AUTO_VOICE: process.env.AUTO_VOICE || "true", // true or false
 AUTO_STICKER: process.env.AUTO_STICKER || "true", // true or false
 AUTO_REPLY: process.env.AUTO_REPLY || "true", // true or false
@@ -21,11 +21,11 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "true", // true or false
 AUTO_REACT: process.env.AUTO_REACT || "true", // true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true or false
 FILENAME: process.env.FILENAME || "QUEEN-ESHU-MD-V1",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/xmhW7DJ/6252.png", 
-CHANNEL_NAME: process.env.CHANNEL_NAME || "💗 QUEEN-ESHU-MD-V1 💗",
-NEWSLETTER_ID: process.env.NEWSLETTER_ID || "120363303242803015@newsletter",
-TITLE: process.env.TITLE || "QUEEN-ESHU-MD-V1",
-BODY: process.env.BODY || "CREATED BY ESHAN TECH SL🛡️",
-WEBURL: process.env.WEBURL|| "https://github.com/CYBER-ESHU/ESHU-MD",
-BOT_IMG: process.env.BOT_IMG || "https://i.ibb.co/xmhW7DJ/6252.png"     
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nWJqfPb/mrgmax.jpg", 
+CHANNEL_NAME: process.env.CHANNEL_NAME || "𝐐𝐔𝐄𝐄𝐍 𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐕2",
+NEWSLETTER_ID: process.env.NEWSLETTER_ID || "120363378635880298@newsletter",
+TITLE: process.env.TITLE || "𝐐𝐔𝐄𝐄𝐍 𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐕2",
+BODY: process.env.BODY || "CREATED BY AKINDU 🧚‍♂️",
+WEBURL: process.env.WEBURL|| "",
+BOT_IMG: process.env.BOT_IMG || "https://i.ibb.co/nWJqfPb/mrgmax.jpg"     
 };
